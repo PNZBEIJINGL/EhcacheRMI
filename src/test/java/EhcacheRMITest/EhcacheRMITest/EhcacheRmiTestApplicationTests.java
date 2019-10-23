75727d39c129a36f582cc20210681be63ddf695b
liuyong <liuyong@startimes.com.cn>
2019-10-23 20:18:49 +0800

    2018

---
+++ src/test/java/EhcacheRMITest/EhcacheRMITest/EhcacheRmiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package EhcacheRMITest.EhcacheRMITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhcacheRmiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
