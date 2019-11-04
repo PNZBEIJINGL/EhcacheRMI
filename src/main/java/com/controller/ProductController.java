bcd8d52eca33706e4953837e40f52aa22e4c9f8e
liuyong <liuyong@startimes.com.cn>
2019-11-04 09:30:44 +0800

    0930

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,22 +1,52 @@
 package com.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class ProductController {
 
-    @GetMapping("/product")
-    public String getProduct() {
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("PRODUCTID=1");
-        stringBuffer.append("|");
-        return stringBuffer.toString();
+    @GetMapping("/save")
+    public String saveProduct() {
+        // 1. 创建缓存管理器
+        CacheManager cacheManager = CacheManager.create("./src/main/resources/ehcache.xml");
+        // 2. 获取缓存对象
+        Cache cache = cacheManager.getCache("daoCache");
+        // 3. 创建元素
+        for (int i = 0; i < 5; i++) {
+            Element element = new Element(i, "product " + i);
+            // 4. 将元素添加到缓存
+            cache.put(element);
+        }
+
+        return "success";
+
     }
 
+    @GetMapping("/update")
+    public String updateProduct() {
+        CacheManager cacheManager = CacheManager.create("./src/main/resources/ehcache.xml");
+        // 2. 获取缓存对象
+        Cache cache = cacheManager.getCache("daoCache");
+        Element element = new Element(1, "update product " + 1);
+        cache.put(element);
+        return "success";
+    }
+
+    @GetMapping("/get")
+    public String getProduct() {
+        CacheManager cacheManager = CacheManager.create("./src/main/resources/ehcache.xml");
+        // 2. 获取缓存对象
+        Cache cache = cacheManager.getCache("daoCache");
+        Element e = cache.get(1);
+        return e.getObjectValue().toString();
+    }
 
     @RequestMapping("/")
     public String index() {
